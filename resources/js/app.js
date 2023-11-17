f0a942ffd40c9555788e5ca5e18f573fba6f1c09
BrianNyaberi <briannyaberi.com>
2023-11-17 14:16:22 +0300

    Authentication feature

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,44 +1,51 @@
-import './bootstrap';
+import './bootstrap.js';
 
-import Vue from "vue";
-import App from "./vue/app";
-import VueRouter from 'vue-router';
+import { createApp } from "vue";
+import welcome from "./components/welcome.vue";
 
-import Login from './components/Login.vue';
-import TimesPage from './components/TimesPage.vue';
-import addItemForm from './addItemForm';
-import listView from './listView';
+const app = createApp({})
+app.component('welcome', welcome)
+app.mount('#app')
 
-Vue.use(VueRouter);
 
-export default {
-  components: {
-    addItemForm,
-    listView
-  }
-};
+// import App from "./vue/app";
+// import VueRouter from 'vue-router';
 
-const router = new VueRouter({
-  routes: [
-    { path: '/', component: Login },
-    { path: '/times', component: TimesPage, meta: { requiresAuth: true } }
-  ]
-});
+// import Login from './components/Login.vue';
+// import TimesPage from './components/TimesPage.vue';
+// import addItemForm from './addItemForm';
+// import listView from './listView';
 
-router.beforeEach((to, from, next) => {
-  const isLoggedIn = /* Logic to check if the user is logged in */;
-  if (to.matched.some(record => record.meta.requiresAuth)) {
-    if (!isLoggedIn) {
-      next('/');
-    } else {
-      next();
-    }
-  } else {
-    next();
-  }
-});
+// Vue.use(VueRouter);
 
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app');
+// export default {
+//   components: {
+//     addItemForm,
+//     listView
+//   }
+// };
+
+// const router = new VueRouter({
+//   routes: [
+//     { path: '/', component: Login },
+//     { path: '/times', component: TimesPage, meta: { requiresAuth: true } }
+//   ]
+// });
+
+// router.beforeEach((to, from, next) => {
+//   // const isLoggedIn = /* Logic to check if the user is logged in */;
+//   if (to.matched.some(record => record.meta.requiresAuth)) {
+//     if (!isLoggedIn) {
+//       next('/');
+//     } else {
+//       next();
+//     }
+//   } else {
+//     next();
+//   }
+// });
+
+// new Vue({
+//   router,
+//   render: h => h(App)
+// }).$mount('#app');
