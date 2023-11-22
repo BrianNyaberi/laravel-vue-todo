ff0496ec46d67e4fa2ddd88971df6430207d3c75
BrianNyaberi <briannyaberi.com>
2023-11-22 10:18:00 +0300

    Registration Data

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,8 +8,7 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Http\Request;
 use App\Models\User;
 
-
-class Controller extends BaseController
+class UserController extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
 
@@ -32,33 +31,33 @@ class Controller extends BaseController
         return response()->json(['user' => $user], 201);
     }
 
-    public function show($id)
-    {
-        $item = User::findOrFail($id);
+    // public function show($id)
+    // {
+    //     $item = User::findOrFail($id);
 
-        return response()->json(['item' => $item], 200);
-    }
+    //     return response()->json(['item' => $item], 200);
+    // }
 
-    public function update(Request $request, $id)
-    {
-        $item = User::findOrFail($id);
+    // public function update(Request $request, $id)
+    // {
+    //     $item = User::findOrFail($id);
 
-        $validatedData = $request->validate([
-            'name' => 'required|string|max:255',
-            'description' => 'nullable|string',
-            // Add other validation rules as needed
-        ]);
+    //     $validatedData = $request->validate([
+    //         'name' => 'required|string|max:255',
+    //         'description' => 'nullable|string',
+    //         // Add other validation rules as needed
+    //     ]);
 
-        $item->update($validatedData);
+    //     $item->update($validatedData);
 
-        return response()->json(['message' => 'Item updated successfully'], 200);
-    }
+    //     return response()->json(['message' => 'Item updated successfully'], 200);
+    // }
 
-    public function destroy($id)
-    {
-        $item = User::findOrFail($id);
-        $item->delete();
+    // public function destroy($id)
+    // {
+    //     $item = User::findOrFail($id);
+    //     $item->delete();
 
-        return response()->json(['message' => 'Item deleted successfully'], 200);
-    }
+    //     return response()->json(['message' => 'Item deleted successfully'], 200);
+    // }
 }
--- routes/api.php
+++ routes/api.php
@@ -23,7 +23,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 // User routes
-Route::post('/register', [UserController::class, 'index']);
+Route::post('/register', [UserController::class, 'store']);
 Route::post('/login', [UserController::class, 'login']);
 
 // items
